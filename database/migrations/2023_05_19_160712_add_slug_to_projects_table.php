3b3baefdba465096e7522bf704438c6fa2ea5c73
christianarca27 <christian.arca.mail@gmail.com>
2023-05-19 19:37:39 +0200

    refactor: spostate nuove colonne in posizioni più adatte

--- database/migrations/2023_05_19_160712_add_slug_to_projects_table.php
+++ database/migrations/2023_05_19_160712_add_slug_to_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->string('slug')->unique();
+            $table->string('slug')->unique()->after('title');
         });
     }
 
