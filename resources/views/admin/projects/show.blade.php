8ef5e56bcd3f0a6a69401dec46ebf19b1f021d45
christianarca27 <christian.arca.mail@gmail.com>
2023-05-20 15:43:11 +0200

    feat: aggiunto modale bootstrap per conferma eliminazione di un progetto

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -15,12 +15,31 @@
         <div class="action d-flex gap-3 mb-3">
             <a class="btn btn-primary" href="{{ route('admin.projects.edit', $project) }}">Modifica progetto</a>
 
-            <form action="{{ route('admin.projects.destroy', $project) }}" method="post">
-                @csrf
-                @method('DELETE')
+            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">Elimina
+                progetto</button>
+        </div>
 
-                <button class="btn btn-danger" type="submit">Elimina progetto</button>
-            </form>
+        <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="deleteModallLabel">Conferma eliminazione</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Chiudi"></button>
+                    </div>
+                    <div class="modal-body">
+                        Sei sicuro di voler eliminare questo progetto?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                        <form action="{{ route('admin.projects.destroy', $project) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+
+                            <button type="submit" class="btn btn-danger">Conferma</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <a href="{{ route('admin.projects.index') }}">Torna alla lista completa</a>
